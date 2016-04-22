158f4dbccea7bfc8ef801a449e4d181463bc9ccf
Samuel Fleischer <fleischer.samuel@gmail.com>
2016-04-22 11:10:20 -0700

    Finished homework due in an hour.....

--- hw_3-apr_22/problem_1.py
+++ hw_3-apr_22/problem_1.py
@@ -27,7 +27,7 @@ def get_actual(epsilon):
 def get_composite(epsilon):
     def f(t):
         order_1 = np.exp(-t) - (1+t)*np.exp(-t/epsilon)
-        order_epsilon = (2 - t)*np.exp(-t) - np.exp(-t/epsilon)
+        order_epsilon = (2 - t)*np.exp(-t) - 2*np.exp(-t/epsilon)
         return order_1 + epsilon*order_epsilon
     def g(t):
         return np.exp(-t) - (1+t)*np.exp(-t/epsilon)
@@ -47,13 +47,15 @@ def main():
         y_composite = composite(x)
         y_first_term = first_term(x)
         error = abs(y_actual - y_composite)
+        relative_error = (y_actual - y_composite)/y_actual
         first_term_error = abs(y_actual - y_first_term)
-        data[str(epsilon)] = [y_actual, y_composite, error, y_first_term, first_term_error]
+        relative_first_term_error = (y_actual - y_first_term)/y_actual
+        data[str(epsilon)] = [y_actual, y_composite, error, y_first_term, first_term_error, relative_error, relative_first_term_error]
 
     plt.figure()
     plt.semilogx(x, data[str(epsilons[0])][0], "-", color="k", linewidth=2, label="actual solution")
     plt.semilogx(x, data[str(epsilons[0])][1], "--", color="0.75", linewidth=2, label=r"$O(\varepsilon)$ composite expansion")
-    plt.semilogx(x, data[str(epsilons[0])][3], "--", color="0.5", linewidth=2, label=r"$(1) composite expansion")
+    plt.semilogx(x, data[str(epsilons[0])][3], "--", color="0.5", linewidth=2, label=r"$O(1)$ composite expansion")
     for epsilon in epsilons:
         plt.semilogx(x, data[str(epsilon)][0], "-", color="k", linewidth=2)
         plt.semilogx(x, data[str(epsilon)][1], "--", color="0.75", linewidth=2)
@@ -65,16 +67,16 @@ def main():
     plt.text(1.5*10**(-4), 0.74, r"$\varepsilon=10^{-4}$")
     plt.text(1.3*10**(-3), 0.7, r"$\varepsilon=10^{-3}$")
     plt.text(10**(-2), 0.6, r"$\varepsilon=10^{-2}$")
-    plt.text(0.8*10**(-1), 0.625, r"$\varepsilon=10^{-1}$", rotation=60)
+    plt.text(0.85*10**(-1), 0.625, r"$\varepsilon=10^{-1}$", rotation=60)
     plt.legend(loc=4, fontsize=10)
-    plt.savefig("semilog.png", filetype="png", dpi=300)
+    plt.savefig("figures/semilog.png", filetype="png", dpi=300)
     plt.close()
     garbage.collect()
 
     plt.figure()
     plt.plot(x, data[str(epsilons[0])][0], "-", color="k", linewidth=2, label="actual solution")
     plt.plot(x, data[str(epsilons[0])][1], "--", color="0.75", linewidth=2, label=r"$O(\varepsilon)$ composite expansion")
-    plt.plot(x, data[str(epsilons[0])][3], "--", color="0.5", linewidth=2, label=r"$(1) composite expansion")
+    plt.plot(x, data[str(epsilons[0])][3], "--", color="0.5", linewidth=2, label=r"$O(1)$ composite expansion")
     for epsilon in epsilons:
         plt.plot(x, data[str(epsilon)][0], "-", color="k", linewidth=2)
         plt.plot(x, data[str(epsilon)][1], "--", color="0.75", linewidth=2)
@@ -86,9 +88,9 @@ def main():
     plt.text(2.5*10**(-2), 0.98, r"$\varepsilon=10^{-4}$")
     plt.text(0.05, 0.88, r"$\varepsilon=10^{-3}$", rotation=-30)
     plt.text(0.14, 0.9, r"$\varepsilon=10^{-2}$", rotation=-30)
-    plt.text(0.1, 0.68, r"$\varepsilon=10^{-1}$", rotation=40)
+    plt.text(0.1, 0.68, r"$\varepsilon=10^{-1}$", rotation=50)
     plt.legend(loc=0, fontsize=10)
-    plt.savefig("standard.png", filetype="png", dpi=300)
+    plt.savefig("figures/standard.png", filetype="png", dpi=300)
     plt.close()
     garbage.collect()
 
@@ -100,30 +102,39 @@ def main():
         plt.ylabel("absolute error")
     plt.title(r"Absolute Error of $O(\varepsilon)$ Composite Expansion")
     plt.text(10**(-1), 8*10**(-10), r"$\varepsilon=10^{-5}$")
-    plt.text(4*10**(-2), 8*10**(-8), r"$\varepsilon=10^{-4}$")
-    plt.text(7*10**(-3), 6.5*10**(-6), r"$\varepsilon=10^{-3}$")
-    plt.text(2*10**(-3), 10**(-2), r"$\varepsilon=10^{-2}$")
-    plt.text(4*10**(-4), 1.1*10**(-1), r"$\varepsilon=10^{-1}$")
-    plt.savefig("errors_epsilon.png", filetype="png", dpi=300)
+    plt.text(10**(-1), 7*10**(-8), r"$\varepsilon=10^{-4}$")
+    plt.text(10**(-1), 6*10**(-6), r"$\varepsilon=10^{-3}$")
+    plt.text(10**(-1), 8*10**(-4), r"$\varepsilon=10^{-2}$")
+    plt.text(10**(-1), 4*10**(-2), r"$\varepsilon=10^{-1}$")
+    plt.savefig("figures/errors_epsilon.png", filetype="png", dpi=300)
     plt.close()
     garbage.collect()
 
     plt.figure()
     for epsilon in epsilons:
-        plt.loglog(x, data[str(epsilon)][4], color="k")
+        plt.loglog(x, data[str(epsilon)][5], color="k")
         plt.ylim([0, 1.1])
         plt.xlabel(r"$x$")
         plt.ylabel("absolute error")
     plt.title(r"Absolute Error of $O(1)$ Composite Expansion")
-    plt.text(10**(-1), 2*10**(-5), r"$\varepsilon=10^{-5}$")
-    plt.text(10**(-1), 2*10**(-4), r"$\varepsilon=10^{-4}$")
-    plt.text(10**(-1), 2*10**(-3), r"$\varepsilon=10^{-3}$")
-    plt.text(10**(-1), 2*10**(-2), r"$\varepsilon=10^{-2}$")
-    plt.text(10**(-1), 2*10**(-1), r"$\varepsilon=10^{-1}$")
-    plt.savefig("errors_1.png", filetype="png", dpi=300)
+    plt.text(10**(-1), 8*10**(-10), r"$\varepsilon=10^{-5}$")
+    plt.text(10**(-1), 8*10**(-8), r"$\varepsilon=10^{-4}$")
+    plt.text(10**(-1), 8*10**(-6), r"$\varepsilon=10^{-3}$")
+    plt.text(10**(-1), 8*10**(-4), r"$\varepsilon=10^{-2}$")
+    plt.text(10**(-1), 5*10**(-2), r"$\varepsilon=10^{-1}$")
+    plt.savefig("figures/errors_1.png", filetype="png", dpi=300)
     plt.close()
     garbage.collect()
 
+    # plt.figure()
+    # for epsilon in epsilons:
+    #     plt.semilogy(x, data[str(epsilon)][4], color="k")
+    #     plt.ylim([0, 1.1])
+    #     plt.xlabel(r"$x$")
+    #     plt.ylabel("relative error")
+    # plt.title(r"Relative Error of $O(\varepsilon)$ Composite Expansion")
+    # plt.show()
+    # plt.close()
 
 if __name__ == "__main__":
     main()
